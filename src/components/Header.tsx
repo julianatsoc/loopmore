bc5268c0c171a4079d1bd8db07115041ef4d9cbe
Juliana <julianatsoc28@gmail.com>
2025-02-23 06:50:09 -0300

    feat: insere playlist da taylor swift

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,14 +2,12 @@ import LoopMoreLogo from "../assets/LoopMoreLogo.svg";
 
 export default function Header() {
   return (
-    <div>
-      <div>
-        <div className="flex flex-col items-center space-y-4  w-full p-4">
-          <img src={LoopMoreLogo} alt="LoopMore Logo" className="w-50 h-50" />
-          <h1 className="text-3xl font-cedarville font-bold text-white">
-            LoopMore
-          </h1>
-        </div>
+    <div className="flex justify-center items-center w-full p-4">
+      <div className="flex items-center space-x-2">
+        <img src={LoopMoreLogo} alt="LoopMore Logo" className="w-20 h-20" />
+        <h1 className="text-3xl font-bold text-[#481818]">
+          Loop<span className="text-[#ECB72C]">More</span>
+        </h1>
       </div>
     </div>
   );
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -19,16 +19,55 @@ const Main = () => {
       />
       <div className="relative z-10 flex flex-col items-center justify-center h-4/5 ">
         <div
-          className="mt-16 flex gap-4 flex-col items-center justify-center bg-glass shadow-glass rounded-lg w-2xl
- h-auto border-glass backdrop-blur-[10px] p-12"
+          className="mt-35 flex gap-4 flex-col items-center justify-center bg-glass shadow-glass rounded-lg w-2xl
+ h-auto border-glass backdrop-blur-[15px] p-12"
         >
           <Header />
           <AudioPlayer audioUrl={TypingNoise} audioTitle="Typing Noise" />
           <AudioPlayer audioUrl={BrownNoise} audioTitle="Brown Noise" />
           <AudioPlayer audioUrl={CafeNoise} audioTitle="Cafe Ambience" />
           <AudioPlayer audioUrl={RainNoise} audioTitle="Rain Noise" />
+
+          <iframe
+            src="https://open.spotify.com/embed/album/6AORtDjduMM3bupSWzbTSG?utm_source=generator"
+            width="100%"
+            height="152"
+            allowfullscreen=""
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+            loading="lazy"
+          ></iframe>
         </div>
       </div>
+      <style jsx>{`
+        input[type="range"] {
+          -webkit-appearance: none;
+          appearance: none;
+          width: 100%;
+          height: 8px;
+          border-radius: 8px;
+          background: #444444;
+        }
+
+        input[type="range"]::-webkit-slider-thumb {
+          -webkit-appearance: none;
+          appearance: none;
+          background-color: white;
+          border-radius: 50%;
+          width: 20px;
+          height: 20px;
+          cursor: pointer;
+          border: 2px solid #ecb72c;
+        }
+
+        input[type="range"]::-moz-range-thumb {
+          background-color: white;
+          border-radius: 50%;
+          width: 20px;
+          height: 20px;
+          cursor: pointer;
+          border: 2px solid #ecb72c;
+        }
+      `}</style>
     </div>
   );
 };
